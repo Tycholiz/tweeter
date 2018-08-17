c585752852034e5bf780bf3a004fda76b876f1a7
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-17 18:40:04 +0000

    last step

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -7,6 +7,8 @@ const tweetsRoutes  = express.Router();
 
 module.exports = function(DataHelpers) {
 
+  //this get route will receive tweets
+  //this route handler calls DataHelpers.getTweets()
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -17,14 +19,14 @@ module.exports = function(DataHelpers) {
     });
   });
 
+  //this get route will create new tweets
+  //this route handler calls DataHelpers.saveTweet()
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
     
-     
-
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
